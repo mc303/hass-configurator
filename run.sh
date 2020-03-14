b68bbfb9d086b8436dce62ccd008882715517567
mc303 <daniel@buonocore.nl>
2020-03-14 11:46:37 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 if [ ! -f /config/settings.conf ]; then
     echo "No configuration file found proceeding with default values"
-    python3 /app/configurator.py
+    python3 /configurator.py
 else
     echo "Configuration file found starting configurator with provided settings"
-    python3 /app/configurator.py /config/settings.conf
+    python3 /configurator.py /config/settings.conf
 fi
