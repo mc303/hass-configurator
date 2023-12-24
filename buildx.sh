f4c967bd785c4f08435727ede413b69443602315
mc303 <daniel@buonocore.nl>
2023-12-24 11:28:14 +0100

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -1,23 +1,42 @@
 #!/usr/bin/env bash
 
-CONTAINER_NAME="mc303/hass-configurator:latest"
-BUIILDX_REPO='build-hass-configurator'
-BUILD_PLATFORM="linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
+CHECK_GITHUB_REPO=danielperna84/hass-configurator
+GITHUB_REPO=hass-configurator
+GITHUB_API_URL=https://api.github.com/repos/${CHECK_GITHUB_REPO}/releases/latest
+GITHUB_REPO_VERSION=$(curl --silent ${GITHUB_API_URL} | jq -r '.tag_name')
+CONTAINER_CURRENT_VERSION=$(docker run --rm quay.io/skopeo/stable list-tags docker://ghcr.io/mc303/${GITHUB_REPO} | jq '.Tags[-1]' | tr -d '"')
+CONTAINER_NAME="ghcr.io/mc303/${GITHUB_REPO}:latest"
+CONTAINER_NAME_TAG_VERSION="ghcr.io/mc303/${GITHUB_REPO}:${CONTAINER_VERSION}"
+BUIILDX_REPO="build-${GITHUB_REPO}" 
+BUILD_PLATFORM="linux/amd64,linux/arm/v7,linux/arm64"
 
-echo ${CONTAINER_NAME}
+echo ${CHECK_GITHUB_REPO}
+echo ${GITHUB_REPO}
+echo ${DOCKER_API_URL}
+echo ${GITHUB_REPO_VERSION}
+echo ${CONTAINER_CURRENT_VERSION}
+echo ${DOCKER_REPO}
+echo ${CONTAINER_NAME_TAG_VERSION}
 echo ${BUIILDX_REPO}
 echo ${BUILD_PLATFORM}
 
-echo "docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --push"
+if [ ${CONTAINER_CURRENT_VERSION} != ${GITHUB_REPO_VERSION} ]
+then
+  echo "docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --push"
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
-#create multiplatform environment
-docker buildx create --platform=${BUILD_PLATFORM} --name ${BUIILDX_REPO}
-docker buildx use ${BUIILDX_REPO}
+  #create multiplatform environment
+  docker buildx create --platform=${BUILD_PLATFORM} --name ${BUIILDX_REPO}
+  docker buildx use ${BUIILDX_REPO}
 
-#build multiplatform docker image
-docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --push
+  #build multiplatform docker image
+  docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --push
 
-#remove multiplatform environment
-docker buildx rm ${BUIILDX_REPO}
+  #remove multiplatform environment
+  docker buildx rm ${BUIILDX_REPO}
+
+else
+  echo "versions are the same nothing to do here.....!!"
+
+fi
